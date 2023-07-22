12ae7e5e625df374594abef8e18612d8247217bb
HtetKo510217 <htetko510217@gmail.com>
2023-07-22 22:01:00 +0630

    Add Cart component and show selected item

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom"
 import NavBar from "./components/navbar/NavBar"
 import HomePage from './components/home/HomePage'
 import ItemDetail from "./components/itemDetail/ItemDetail"
+import Cart from "./components/cart/Cart"
 export default function App() {
   return (
     <div className="app">
@@ -11,6 +12,7 @@ export default function App() {
         </header>
         <Routes>
           <Route path="/item/:id" element={<ItemDetail />} />
+          <Route path="/cart" element={<Cart />} />
           <Route exact path="/" element={<HomePage/>} />
         </Routes>
       </BrowserRouter>
